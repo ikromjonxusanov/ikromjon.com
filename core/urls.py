801011c8cf112b5ba51ada08f9b07b89b20a7ab8
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2024-12-06 14:34:49 +0100

    temporary remove project and blog sections

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ from core.views import home, about, projects, project, blogs, blog
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'),
-    path('projects', projects, name='projects'),
-    path('projects/<int:pk>', project, name='project-detail'),
-    path('blogs', blogs, name='blogs'),
-    path('blogs/<int:pk>', blog, name='blog-detail'),
+    # path('projects', projects, name='projects'),
+    # path('projects/<int:pk>', project, name='project-detail'),
+    # path('blogs', blogs, name='blogs'),
+    # path('blogs/<int:pk>', blog, name='blog-detail'),
 ]
